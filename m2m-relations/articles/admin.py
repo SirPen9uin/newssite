8b2513cdc12a629eae4f858865172b63fdadc8ca
Pen9uin <sirpen9uin@gmail.com>
2024-01-30 15:00:43 +0300

    init commit

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                main_count += 1
+            if main_count > 1 or main_count < 1:
+                raise ValidationError('Основное поле может быть только одно')
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
